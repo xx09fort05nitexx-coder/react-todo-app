53e67555d49a65003491d3e4f95f8003fa44b219
あっつー！ <xx09fort05nitexx@gmail.com>
2026-08-16 19:19:46 +0900

    TODO入力欄の状態管理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import './App.css'
 import { useState } from 'react'
 function App() {
   const [inputText,setInputText] = useState('')
-  
+
   return(
   <>
   <h1>アッツーのTODOアプリ</h1>
 
-  <input type="text" placeholder='TODOを入力'/>
+  <input type="text" placeholder='TODOを入力'
+  value={inputText}
+  onChange={(event) => {
+    setInputText(event.target.value)
+  }}
+  />
+
   <button>追加</button>
   </>
   )
