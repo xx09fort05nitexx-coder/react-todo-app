7c6fca28ba0b5ffb2297178b532b912804922a83
あっつー！ <xx09fort05nitexx@gmail.com>
2026-08-18 00:19:28 +0900

    TODOの追加と一覧表示を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 
 import './App.css'
 import { useState } from 'react'
+
+  type Todo = {
+    id: number
+    text: string
+    completed: boolean
+  }
+
 function App() {
   const [inputText,setInputText] = useState('')
+   const [todos, setTodos] = useState<Todo[]>([])
 
+  const handleAddTodo = () =>{
+    const newTodo: Todo ={
+      id: Date.now(),
+      text: inputText,
+      completed: false      
+    }
+
+    setTodos([...todos,newTodo])
+    
+  }
+
+ 
   return(
   <>
   <h1>アッツーのTODOアプリ</h1>
@@ -15,8 +35,18 @@ function App() {
   }}
   />
 
-  <button>追加</button>
+  <button onClick={handleAddTodo}>追加</button>
+  
+   <ul>
+    {todos.map((todo) => (
+      <li key={todo.id}>
+        {todo.text} 
+      </li>
+    ))}
+  </ul>
   </>
+
+ 
   )
 }
 
